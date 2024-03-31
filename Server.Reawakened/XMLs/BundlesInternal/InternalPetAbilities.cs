4124979c1e87c6faac895691a4ecdffa4e1af8a3
Zachary Baldwin <zacharybaldwin904@gmail.com>
2024-03-31 00:26:09 -0500

    Update PetAbilities

--- Server.Reawakened/XMLs/BundlesInternal/InternalPetAbilities.cs
+++ Server.Reawakened/XMLs/BundlesInternal/InternalPetAbilities.cs
@@ -1,5 +1,4 @@
-﻿using A2m.Server;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 using PetDefines;
 using Server.Base.Core.Extensions;
 using Server.Reawakened.XMLs.Abstractions;
@@ -9,12 +8,10 @@ using System.Xml;
 namespace Server.Reawakened.XMLs.BundlesInternal;
 public class InternalPetAbilities : PetAbilitiesXML, IBundledXml<InternalPetAbilities>
 {
-    public ILogger<InternalPetAbilities> Logger { get; set; }
-
     public string BundleName => "PetAbilities";
-
     public BundlePriority Priority => BundlePriority.Lowest;
 
+    public ILogger<InternalPetAbilities> Logger { get; set; }
     public IServiceProvider Services { get; set; }
 
     public Dictionary<int, PetAbilityParams> AbilityData;
@@ -35,15 +32,4 @@ public class InternalPetAbilities : PetAbilitiesXML, IBundledXml<InternalPetAbil
 
     public void FinalizeBundle() => 
         AbilityData = (Dictionary<int, PetAbilityParams>)this.GetField<PetAbilitiesXML>("_petAbilityData");
-
-    public new PetAbilityParams GetAbilityData(int prefabID)
-    {
-        if (!AbilityData.TryGetValue(prefabID, out var value))
-        {
-            Logger.LogWarning("Pet with id {prefabId} does not exist in AbilityData", prefabID);
-            return null;
-        }
-
-        return value;
-    }
 }
